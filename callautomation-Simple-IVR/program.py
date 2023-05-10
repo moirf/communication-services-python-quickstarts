e08fd73f271e101ca7026d931f51897fd12b0225
maulinasharma <v-mausha@microsoft.com>
2023-05-10 16:12:00 +0530

    refactor code

--- callautomation-Simple-IVR/program.py
+++ callautomation-Simple-IVR/program.py
@@ -2,23 +2,21 @@ import re
 import azure
 import ast
 from azure.eventgrid import EventGridEvent
-from azure.communication.identity._shared.models import CommunicationIdentifier,PhoneNumberIdentifier,\
+from azure.communication.identity._shared.models import PhoneNumberIdentifier,\
     CommunicationUserIdentifier,CommunicationIdentifierKind,identifier_from_raw_id
 import json
 from aiohttp import web
 from Logger import Logger
 from ConfigurationManager import ConfigurationManager
-from azure.communication.identity import CommunicationIdentityClient
 from azure.communication.callautomation import CallAutomationClient,CallInvite,\
 CallAutomationEventParser,CallConnected,CallMediaRecognizeDtmfOptions,\
 FileSource,DtmfTone,PlayCompleted,PlayFailed,AddParticipantSucceeded,AddParticipantFailed,\
 RecognizeCompleted,RecognizeFailed\
-    
 
 configuration_manager = ConfigurationManager.get_instance()
 calling_Automation_client = CallAutomationClient.from_connection_string(configuration_manager.get_app_settings("Connectionstring"))
-BaseUri=configuration_manager.get_app_settings('BaseUri')
-callerId = None
+base_uri=configuration_manager.get_app_settings('base_uri')
+caller_id = None
 user_identity_regex: str = "8:acs:[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}_[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}"
 phone_identity_regex: str = "^\\+\\d{10,14}$"
 
@@ -64,9 +62,9 @@ class Program():
                                 return web.Response(text=str(ex), status=500)   
                     
                     callerId = str(event_data['from']['rawId'])                   
-                    incomingCallContext = event_data['incomingCallContext']                   
-                    callbackUri = BaseUri + '/api/calls/callerId=?'+ callerId
-                    AnswerCallResult = calling_Automation_client.answer_call(incomingCallContext, callbackUri)
+                    incoming_call_context = event_data['incomingCallContext']                   
+                    callback_uri = base_uri + '/api/calls/callerId=?'+ callerId
+                    answer_call_result = calling_Automation_client.answer_call(incoming_call_context, callback_uri)
                     return web.Response(status=200)
             except Exception as ex:
                     Logger.log_message(
@@ -79,13 +77,12 @@ class Program():
     
     async def start_callBack(self,request):
         try: 
-             
              content = await request.content.read()
              event = CallAutomationEventParser.parse(content)
              Logger.log_message(Logger.INFORMATION,'Event Kind IS :' 
                                  + event.kind)                         
-             call_Connection = calling_Automation_client.get_call_connection(event.call_connection_id)            
-             call_Connection_Media =call_Connection.get_call_media()            
+             call_connection = calling_Automation_client.get_call_connection(event.call_connection_id)            
+             call_connection_media =call_connection.get_call_media()            
              if event.__class__ == CallConnected:
                  Logger.log_message(Logger.INFORMATION,'CallConnected event received for call connection id --> ' 
                                  + event.call_connection_id)                
@@ -94,42 +91,42 @@ class Program():
                  recognize_Options.interrupt_prompt = True
                  recognize_Options.inter_tone_timeout = 30                 
                  recognize_Options.initial_silence_timeout=5 
-                 File_source=FileSource(uri=(BaseUri + configuration_manager.get_app_settings('MainMenuAudio')))                 
+                 File_source=FileSource(uri=(base_uri + configuration_manager.get_app_settings('MainMenuAudio')))                 
                  File_source.play_source_id= 'MainMenu'                 
                  recognize_Options.play_prompt = File_source                
                  recognize_Options.operation_context= 'MainMenu'             
-                 call_Connection_Media.start_recognizing(recognize_Options)
+                 call_connection_media.start_recognizing(recognize_Options)
                  
              if event.__class__ == RecognizeCompleted and event.operation_context == 'MainMenu' :
                  Logger.log_message(Logger.INFORMATION,'RecognizeCompleted event received for call connection id --> '+ event.call_connection_id
                                     +'Correlation id:'+event.correlation_id)
-                 toneDetected=event.collect_tones_result.tones[0]
-                 if toneDetected == DtmfTone.ONE:
-                     play_source = FileSource(uri=(BaseUri + configuration_manager.get_app_settings('SalesAudio')))
+                 tone_detected=event.collect_tones_result.tones[0]
+                 if tone_detected == DtmfTone.ONE:
+                     play_source = FileSource(uri=(base_uri + configuration_manager.get_app_settings('SalesAudio')))
                      play_source.play_source_id='SimpleIVR'
-                     call_Connection_Media.play_to_all(play_source,operation_context='SimpleIVR')
+                     call_connection_media.play_to_all(play_source,operation_context='SimpleIVR')
                     
-                 elif toneDetected == DtmfTone.TWO :
-                       play_source = FileSource(uri=(BaseUri + configuration_manager.get_app_settings('MarketingAudio')))
-                       call_Connection_Media.play_to_all(play_source,operation_context='SimpleIVR')
-                 elif toneDetected == DtmfTone.THREE :
-                       play_source = FileSource(uri=(BaseUri + configuration_manager.get_app_settings('CustomerCareAudio')))
-                       call_Connection_Media.play_to_all(play_source, operation_context='SimpleIVR')
-                 elif toneDetected == DtmfTone.FOUR :
-                       play_source = FileSource(uri=(BaseUri + configuration_manager.get_app_settings('AgentAudio')))
-                       call_Connection_Media.play_to_all(play_source,  operation_context='AgentConnect')
-                 elif toneDetected == DtmfTone.FIVE :
-                      call_Connection.hang_up(True)
+                 elif tone_detected == DtmfTone.TWO :
+                       play_source = FileSource(uri=(base_uri + configuration_manager.get_app_settings('MarketingAudio')))
+                       call_connection_media.play_to_all(play_source,operation_context='SimpleIVR')
+                 elif tone_detected == DtmfTone.THREE :
+                       play_source = FileSource(uri=(base_uri + configuration_manager.get_app_settings('CustomerCareAudio')))
+                       call_connection_media.play_to_all(play_source, operation_context='SimpleIVR')
+                 elif tone_detected == DtmfTone.FOUR :
+                       play_source = FileSource(uri=(base_uri + configuration_manager.get_app_settings('AgentAudio')))
+                       call_connection_media.play_to_all(play_source,  operation_context='AgentConnect')
+                 elif tone_detected == DtmfTone.FIVE :
+                      call_connection.hang_up(True)
                  else:
-                     play_source = FileSource(uri=(BaseUri + configuration_manager.get_app_settings('InvalidAudio')))
-                     call_Connection_Media.play_to_all(play_source, operation_context='SimpleIVR')                
+                     play_source = FileSource(uri=(base_uri + configuration_manager.get_app_settings('InvalidAudio')))
+                     call_connection_media.play_to_all(play_source, operation_context='SimpleIVR')                
                            
                  
              if event.__class__ == RecognizeFailed and event.operation_context == 'MainMenu' :
                  Logger.log_message(Logger.INFORMATION,'Recognition timed out for call connection id --> '+ event.call_connection_id
                                     +'Correlation id:'+event.correlation_id)
-                 play_source = FileSource(uri=(BaseUri + configuration_manager.get_app_settings('InvalidAudio')))
-                 call_Connection_Media.play_to_all(play_source,operation_context='SimpleIVR')
+                 play_source = FileSource(uri=(base_uri + configuration_manager.get_app_settings('InvalidAudio')))
+                 call_connection_media.play_to_all(play_source,operation_context='SimpleIVR')
              if event.__class__ == PlayCompleted:
                  
                  if event.operation_context == 'AgentConnect':
@@ -140,33 +137,33 @@ class Program():
                             Participant_Identity = get_identifier_kind(participant_to_add)                
                             if Participant_Identity == CommunicationIdentifierKind.COMMUNICATION_USER :
                                 self.Participant_Add=CommunicationUserIdentifier(participant_to_add)
-                                Callinvite=CallInvite(self.Participant_Add)                    
+                                call_invite=CallInvite(self.Participant_Add)                    
                             if Participant_Identity == CommunicationIdentifierKind.PHONE_NUMBER :
                                 self.Participant_Add=PhoneNumberIdentifier(participant_to_add)
-                                Callinvite=CallInvite(self.Participant_Add,sourceCallIdNumber=self.source_identity)                    
+                                call_invite=CallInvite(self.Participant_Add,sourceCallIdNumber=self.source_identity)                    
                                         
                             Logger.log_message(Logger.INFORMATION,'Performing add Participant operation')
-                            self.add_participant_response=call_Connection.add_participant(Callinvite)                            
+                            self.add_participant_response=call_connection.add_participant(call_invite)                            
                             Logger.log_message(
                             Logger.INFORMATION, 'Call initiated with Call Leg id -- >' + self.add_participant_response.participant)
                                          
                  if event.operation_context == 'SimpleIVR':
                      Logger.log_message(Logger.INFORMATION,'PlayCompleted event received for call connection id --> '+ event.call_connection_id
                                     +'Call Connection Properties :'+event.correlation_id)
-                     call_Connection.hang_up(True)
+                     call_connection.hang_up(True)
                      
              if event.__class__ == PlayFailed:
                      Logger.log_message(Logger.INFORMATION,'PlayFailed event received for call connection id --> '+ event.call_connection_id
                                     +'Call Connection Properties :'+event.correlation_id)
-                     call_Connection.hang_up(True) 
+                     call_connection.hang_up(True) 
              if event.__class__ == AddParticipantSucceeded:
                      Logger.log_message(Logger.INFORMATION,'AddParticipantSucceeded event received for call connection id --> '+ event.call_connection_id
                                     +'Call Connection Properties :'+event.correlation_id)
-                     call_Connection.hang_up(True)
+                     call_connection.hang_up(True)
              if event.__class__ == AddParticipantFailed:
                      Logger.log_message(Logger.INFORMATION,'AddParticipantFailed event received for call connection id --> '+ event.call_connection_id
                                     +'Call Connection Properties :'+event.correlation_id)
-                     call_Connection.hang_up(True)                         
+                     call_connection.hang_up(True)                         
               
         except Exception as ex:
             Logger.log_message(
